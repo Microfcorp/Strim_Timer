dad2093acf9e31247a20ddb62396e534f3722b26
Microfcorp <a.mostowenko2012@yandex.ru>
2018-01-03 00:49:50 +0300

    Add files via upload

---
+++ Stream_Timer/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace Stream_Timer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.настройкиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.путьКВыходномуФайлуToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripTextBox1 = new System.Windows.Forms.ToolStripTextBox();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.выбратьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(187, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(274, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Обратный отсчёт для трансляции";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 90);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(176, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(120, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Текст перед отсчётом";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 189);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(229, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Запустить отсчёт";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(206, 90);
+            this.maskedTextBox1.Mask = "00:00:00";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(50, 20);
+            this.maskedTextBox1.TabIndex = 4;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.настройкиToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(631, 24);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // настройкиToolStripMenuItem
+            // 
+            this.настройкиToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.путьКВыходномуФайлуToolStripMenuItem});
+            this.настройкиToolStripMenuItem.Name = "настройкиToolStripMenuItem";
+            this.настройкиToolStripMenuItem.Size = new System.Drawing.Size(79, 20);
+            this.настройкиToolStripMenuItem.Text = "Настройки";
+            // 
+            // путьКВыходномуФайлуToolStripMenuItem
+            // 
+            this.путьКВыходномуФайлуToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripTextBox1,
+            this.toolStripSeparator1,
+            this.выбратьToolStripMenuItem});
+            this.путьКВыходномуФайлуToolStripMenuItem.Name = "путьКВыходномуФайлуToolStripMenuItem";
+            this.путьКВыходномуФайлуToolStripMenuItem.Size = new System.Drawing.Size(212, 22);
+            this.путьКВыходномуФайлуToolStripMenuItem.Text = "Путь к выходному файлу";
+            // 
+            // toolStripTextBox1
+            // 
+            this.toolStripTextBox1.Name = "toolStripTextBox1";
+            this.toolStripTextBox1.ReadOnly = true;
+            this.toolStripTextBox1.Size = new System.Drawing.Size(100, 23);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(157, 6);
+            // 
+            // выбратьToolStripMenuItem
+            // 
+            this.выбратьToolStripMenuItem.Name = "выбратьToolStripMenuItem";
+            this.выбратьToolStripMenuItem.Size = new System.Drawing.Size(160, 22);
+            this.выбратьToolStripMenuItem.Text = "Выбрать";
+            this.выбратьToolStripMenuItem.Click += new System.EventHandler(this.выбратьToolStripMenuItem_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 228);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Пред. простомтр";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(22, 126);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(156, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Текст по истечению времени";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 153);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(176, 20);
+            this.textBox2.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(262, 90);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(315, 26);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Следует учесть, что вермя равное 00:01:00 равное 00:00:00, \r\nа соответственно отс" +
+    "чет производится не будет";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(631, 305);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Stream Timer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem настройкиToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem путьКВыходномуФайлуToolStripMenuItem;
+        private System.Windows.Forms.ToolStripTextBox toolStripTextBox1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem выбратьToolStripMenuItem;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Stream_Timer/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Stream_Timer
+{
+    public partial class Form1 : Form
+    {
+        string filename = Directory.GetCurrentDirectory() + "/Stream_timer.txt";
+        string times;
+        string texts;
+
+        int h;
+        int m;
+        int s;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void выбратьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Title = "Сохраниние файла";
+            //sfd.DefaultExt = ".txt";
+            sfd.Filter = "txt files (*.txt)|*.txt";
+            sfd.FileName = "Stream_timer";
+            sfd.AddExtension = false;
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                filename = sfd.FileName;
+                toolStripTextBox1.Text = filename;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            times = maskedTextBox1.Text;
+            texts = textBox1.Text;
+            DateTime time = DateTime.Parse(times);
+            h = Convert.ToInt32(time.Hour);
+            m = Convert.ToInt32(time.Minute);
+            s = Convert.ToInt32(time.Second);
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Stop();
+
+            DateTime time = DateTime.Parse(times);
+            DateTime localTime = DateTime.Now;
+
+
+            s--;
+            if (s < 1)
+            {
+                s = 59;
+                m--;
+            }
+            if (m < 1)
+            {
+                m = 59;
+                h--;
+            }
+            if (h < 0)
+            {
+                if (filename != "")
+                {
+                    //Pass the filepath and filename to the StreamWriter Constructor
+                    StreamWriter sw1 = new StreamWriter(filename);
+
+                    sw1.WriteLine(textBox2.Text);
+
+                    // sw.WriteLine("From the StreamWriter class");
+                    //Close the file
+                    sw1.Close();
+                }
+                timer1.Stop();
+            }
+            else
+            {
+
+
+                Console.WriteLine("Difference between {0} and {1} time: {2} : {3} hours",
+                                  time.ToString(),
+                                  localTime.ToString(),
+                                  (time - localTime).Hours,
+                                  (time - localTime).Minutes);
+
+                if (filename != "")
+                {
+                    //Pass the filepath and filename to the StreamWriter Constructor
+                    StreamWriter sw = new StreamWriter(filename);
+
+                    sw.WriteLine(textBox1.Text + " " + h + ":" + m + ":" + s);
+
+                    // sw.WriteLine("From the StreamWriter class");
+                    //Close the file
+                    sw.Close();
+                }
+            }
+            label3.Text = "Осталось " + h + ":" + m + ":" + s;
+
+            timer1.Start();
+      }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            toolStripTextBox1.Text = filename;
+        }
+    }
+}
